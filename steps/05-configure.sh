80782c652140cd94e5071bb7bc3aab3bdcec6222
Jordan Coult <jordan@auterion.com>
2024-10-20 21:59:55 -0500

    Revert "Add GDAL-recommended build args"
    This reverts commit 8bd4445b544c77203b18b8b92f434f45362332ee.

--- steps/05-configure.sh
+++ steps/05-configure.sh
@@ -16,20 +16,10 @@ mkdir -p "$BUILD"
   echo "pdf_use_partition_alloc = false"
   echo "target_cpu = \"$TARGET_CPU\""
   echo "target_os = \"$OS\""
-  echo "pdf_enable_v8 = $ENABLE_V8"  # Should be false - no need for JavaScript.
+  echo "pdf_enable_v8 = $ENABLE_V8"
   echo "pdf_enable_xfa = $ENABLE_V8"
   echo "treat_warnings_as_errors = false"
   echo "is_component_build = false"
-  echo "pdf_use_skia = false"        # Disable experimental Skia backend
-  echo "pdf_use_skia_paths = false"  # Disable experimental Skia paths backend
-  echo "pdf_is_complete_lib = true"
-  echo "use_custom_libcxx = false"   # Avoid incompatible namespace std::1::
-  echo "use_rtti = true"             # Enable RTTI for virtual class subclassing
-  echo "use_system_libjpeg = true"
-  echo "use_system_libpng = true"
-  echo "use_system_lcms2 = true"
-  echo "use_system_libopenjpeg2 = true"
-  echo "use_sysroot = false"         # Prevent fetching incompatible headers
 
   if [ "$ENABLE_V8" == "true" ]; then
     echo "v8_use_external_startup_data = false"
@@ -39,7 +29,7 @@ mkdir -p "$BUILD"
   case "$OS" in
     android)
       echo "clang_use_chrome_plugins = false"
-      echo "default_min_sdk_version = 21"  # Can change this if needed
+      echo "default_min_sdk_version = 21"
       ;;
     ios)
       [ -n "$TARGET_ENVIRONMENT" ] && echo "target_environment = \"$TARGET_ENVIRONMENT\""
@@ -56,6 +46,7 @@ mkdir -p "$BUILD"
       echo "clang_use_chrome_plugins = false"
       ;;
     wasm)
+      echo 'pdf_is_complete_lib = true'
       echo 'is_clang = false'
       ;;
   esac
@@ -64,20 +55,18 @@ mkdir -p "$BUILD"
     musl)
       echo 'is_musl = true'
       echo 'is_clang = false'
-      # 'use_custom_libcxx = false' is already set globally
-      if [ "$ENABLE_V8" == "true" ]; then
-        case "$TARGET_CPU" in
-          arm)
+      echo 'use_custom_libcxx = false'
+      [ "$ENABLE_V8" == "true" ] && case "$TARGET_CPU" in
+        arm)
             echo "v8_snapshot_toolchain = \"//build/toolchain/linux:clang_x86_v8_arm\""
             ;;
-          arm64)
+        arm64)
             echo "v8_snapshot_toolchain = \"//build/toolchain/linux:clang_x64_v8_arm64\""
             ;;
-          *)
+        *)
             echo "v8_snapshot_toolchain = \"//build/toolchain/linux:$TARGET_CPU\""
             ;;
-        esac
-      fi
+      esac
       ;;
   esac
 
